138cb442a4a5e40c3f645bc985f2823b443237ee
k-ken-souce <chirag21works@gmail.com>
2020-04-18 03:18:10 +0530

    version2

--- linklist.c
+++ linklist.c
@@ -1,52 +1,139 @@
 #include<stdio.h>
 #include<stdlib.h>
+//pop
+//push
+//insert_at()
 
 struct node{
 	int data;
 	struct node* next;
 };
 
-struct node *head;
+struct node *head=NULL;
 
-void insert_beg(int data)
-{
-	
-		struct node *newNode = (struct node *)malloc(sizeof(struct node));
-		newNode->data=data;
+void printList(){
+	struct node *temp=head;
+	while(temp!=NULL){
+		printf("->%d",temp->data );
+		temp=temp->next;
 
-		if(head == NULL){
-			newNode->next=NULL;
-			head=newNode;
+	}
+}
+
+void delete(int data){
+	struct node * curr=head;
+	struct node * prev=NULL;
+
+	while(curr!=NULL && curr->data!=data){
+		prev=curr;
+		curr=curr->next;
+	}
+	if(curr==NULL){
+		return;
+	}
+
+	//first element
+
+	if(prev==NULL){
+		if(curr->next==NULL){
+			head=NULL;
 		}
 		else{
-			newNode->next=head;
-			head=newNode;
+			head=head->next;
 		}
+	free(curr);
+	return;
+	}
+
+	//last element;
+	if (curr->next==NULL){
+		prev->next=NULL;
+
+		free(curr);
+		return;
+	}
+	//middle element;
+	if(curr->next!=NULL && prev!=NULL){
+		prev->next = curr->next;
+		free(curr);
+		return;
+
+	}
+
 }
 
-void inset_end(int data){
-	struct node * newNode= (struct node *)malloc(sizeof(struct node));
-	newNode->data=data
+void pop(){
+	struct node * curr=head;
+	struct node * prev=NULL;
+
+	while(curr->next!=NULL){
+		prev=curr;
+		curr=curr->next;
+	}
+	prev->next=NULL;
+	free(curr);
+
 }
 
+void insert(int data, int index){
+	int idx=0;
+	struct node * newNode=(struct node *)malloc(sizeof(struct node));
+	struct node * curr=head;
+	struct node * prev=NULL;
 
+	newNode->data = data;
+	if(head==NULL){
+		newNode->next=NULL;
+		head=newNode;
+		return;
+	}
+
+	//at begining;
+	if(index==0){
+		newNode->next = head;
+		head=newNode;
+		return;
+	}
+	//at the end;
+	while(curr!=NULL && idx!=index){
+		prev=curr;
+		curr=curr->next;
+		idx+=1;
+	}
+	if(curr==NULL){
+		prev->next=newNode;
+		newNode->next=NULL;
+		return;
+	}
+	//middle;
+
+	prev->next=newNode;
+	newNode->next=curr;
+
+}
+
+void append(int data){
+	insert(data,-1);
+}
 int main(){
-	
-	head = (struct node *)malloc(sizeof(struct node));
 
-	int data,ch,n,i;
-	printf("Enter the size of list" );
+	int n,i,data;
+	printf("Enter the number of nodes");
 	scanf("%d",&n);
 
 	for(i=0;i<n;i++){
 		scanf("%d",&data);
-		insert(data);
+		append(data);
 	}
+	printList();
 
-	struct node *temp=head;
-	while (temp->next!=	NULL){
-		printf("->%d",temp->data);
-		temp=temp->next;
-	}
+	//insert(23,6);
+	printf("\n");
+	printList();
+	//pop();
+	delete(5);
+	printf("\n");
+	printList();
+
 	return 0;
 }
