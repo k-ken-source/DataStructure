af61689be2b7ae3b5cb471892e9b0d7bb1a3d379
k-ken-souce <chirag21works@gmail.com>
2020-12-02 23:32:17 +0530

    Dec2

--- Python/DivideAndConquor/MaximumSubarrayProblem.py
+++ Python/DivideAndConquor/MaximumSubarrayProblem.py
@@ -1,8 +1,23 @@
 #Find the subarray with maximum sum using divide and conquer technique??
 
-def SubArrayMaxSum(L,n):
+def MaxCross(L,low,high,mid):
     
+
+def SubArrayMaxSum(L,low, high):
+    #Base Conditions 
+    if low == high:
+        return L[low]
+    
+    m = (low+high)//2
+    left  = SubArrayMaxSum(L,low,m)
+    right = SubArrayMaxSum(L,m+1,high)
+
+    cross = MaxCross(L,low,high,mid)
+
+    return max(max(left,right),cross)
+
+
 if if __name__ == "__main__":
     n = int(input("number of elements"))
     arr = list(map(int,input().split()))
-    print(SubArrayMaxSum(arr,n))
+    print(SubArrayMaxSum(arr,0,n))
